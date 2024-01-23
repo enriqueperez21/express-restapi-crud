2d68067f68de444629fa9179e7bab6d17e7d8b23
Luis Enrique Pérez Señalin <105236440+enriqueperez21@users.noreply.github.com>
2024-01-23 17:20:13 -0500

    Add start on package json script

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ require('ejs')
 
 const app = express();
 
-const HomeRoutes = require('./routes/home')
-const UserRoutes = require('./routes/users')
+const HomeRoutes = require('./src/routes/home')
+const UserRoutes = require('./src/routes/users')
 
 //settingg
 app.set('case sensitive routing', true)
 app.set('appName', 'Express course')
 app.set('port', 3000)
 app.set('view engine', 'ejs')
-app.set('views',path.join(__dirname, 'views'))
+app.set('views',path.join(__dirname, './src/views'))
 
 //Middlewares
 app.use(express.json());
@@ -23,8 +23,8 @@ app.use(HomeRoutes)
 app.use(UserRoutes)
 
 //Brindar ruta estática
-app.use("/public",express.static(path.join(__dirname, 'public')))
-app.use("/uploads",express.static(path.join(__dirname, 'uploads')))
+app.use("/public",express.static(path.join(__dirname, './src/public')))
+app.use("/uploads",express.static(path.join(__dirname, './src/uploads')))
 
 app.listen(app.get('port'));
 console.log(`Server ${app.get('appName')} on port ${app.get('port')}`);
